1f936e8d8ca14fbc687d3aa10a21568b3528736b
preet3300-cpu <preet3300@gmail.com>
2026-08-04 03:34:52 +0000

    This is Jocelynn. This is my homework.

--- lesson1/homework/Problems.java
+++ lesson1/homework/Problems.java
@@ -2,22 +2,28 @@ public class Problems {
     public static void main(String[] args) {
         // Problem 1
         // Print your name.
+        System.out.println( "Jocelynn" );
 
-
-        // Problem 2
+        // Problem 2    
         // Create a variable for your lucky number and print it.
-
+        int luckynumber = 2;
+        System.out.println( "My lucky number is " + luckynumber + "." );
 
         // Problem 3
         // Create a variable for a city and print it in a sentence.
-
+        String cityname = "Seattle";
+        System.out.println( "The city I live in is " + cityname + ".");
 
         // Problem 4
         // Create a double variable for a price and print it.
-
+        double price = 10.99;
+        System.out.println( "A gallon of milk costs $ " + price + ".");
 
         // Problem 5
         // Create variables for a movie name and rating.
         // Print them in one sentence.
+        String moviename = "Brand New Day";  
+        String rating = "PG13";
+        System.out.println( "The movie " + moviename + " is rated " + rating );
     }
 }
